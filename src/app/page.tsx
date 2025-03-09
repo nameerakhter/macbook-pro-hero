89227e7f7018e2841dda9d0eb2d53960169d6aec
nameerakhter <akhtarnameer@gmail.com>
2025-03-09 16:17:26 +0530

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,26 @@ import { Environment, ScrollControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { MacbookContainer } from './_components/macbook-container'
 
+const products = ['MacBook Pro', 'iPhone', 'iPad', 'Apple Watch', 'AirPods']
+
 export default function Home() {
   return (
-    <div className="h-screen w-full">
-      <div className="font-(family-name:'Helvetica_Now_Display') absolute top-40 left-1/2 flex -translate-x-1/2 flex-col items-center">
+    <div className="font-(family-name:'Helvetica_Now_Display') h-screen w-full">
+      <nav className="absolute top-0 left-0 w-full py-10">
+        <ul className="flex justify-center space-x-4">
+          {products.map((product) => (
+            <li key={product} className="font-[400] text-white">
+              {product}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="absolute top-40 left-1/2 flex -translate-x-1/2 flex-col items-center px-4 text-center md:px-0">
         <h3 className="masked text-7xl font-[700] tracking-tighter">
           Macbook pro
         </h3>
         <div>
-          <p className="mt-4 text-lg text-gray-600">
+          <p className="mx-auto mt-4 max-w-lg text-lg text-gray-600">
             Experience the unparalleled performance and stunning display of the
             latest MacBook Pro. Power your creativity with the M3 chip and
             immerse yourself in a world of limitless possibilities.
