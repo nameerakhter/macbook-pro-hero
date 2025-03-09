7c578c19a3e5a3ba446e02011266d64f452fd1f1
nameerakhter <akhtarnameer@gmail.com>
2025-03-09 13:53:54 +0530

    test orbitcontrols

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client"
 
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 export default function Home() {
   return (
     <Canvas>
+      <OrbitControls />
+      <mesh>
+        <boxGeometry />
+      </mesh>
     </Canvas>
   );
 }
