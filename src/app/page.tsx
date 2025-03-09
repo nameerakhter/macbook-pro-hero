3f848ca7141df5da5d3b7363fb5fd20ee401d1e7
nameerakhter <akhtarnameer@gmail.com>
2025-03-09 14:01:32 +0530

    impoert macbookcontainer on canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@
 
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { MacbookContainer } from "./_components/macbook-container";
 
 export default function Home() {
   return (
     <Canvas>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-      </mesh>
+      <MacbookContainer />
     </Canvas>
   );
 }
